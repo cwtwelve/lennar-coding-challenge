ceb7f941b90547746e9c8a5d2eedeb1839d0e522
cwtwelve <cwakefield12@gmail.com>
2023-07-29 13:59:29 -0500

    Opacity

--- app/page.tsx
+++ app/page.tsx
@@ -33,50 +33,52 @@ export default function Home() {
 	};
 
 	return (
-		<main className="p-4 page-container">
+		<main className={`p-4 page-container`}>
 			<div
-				className={`text-white flex items-center ${
-					dropDownmenuOpen && "opacity-25"
+				className={`p-4 page-container ${
+					dropDownmenuOpen ? "opacity-25" : ""
 				}`}
 			>
-				{getImage("Logo", 75, 75, "pr-4")}
-				<div className="hidden sm:flex items-center space-x-4">
-					{menuItems.map((menuItem) => (
-						<div key={menuItem}>{menuItem}</div>
-					))}
+				<div className="text-white flex items-center">
+					{getImage("Logo", 75, 75, "pr-4")}
+					<div className="hidden sm:flex items-center space-x-4">
+						{menuItems.map((menuItem) => (
+							<div key={menuItem}>{menuItem}</div>
+						))}
+					</div>
+					<div className="ml-auto hidden sm:flex items-center">
+						<div className="mr-4">Log in</div>
+						<button className="btn bg-gray-600 text-white normal-case font-normal border-none hover:bg-gray-700">
+							Start free trial
+						</button>
+					</div>
+					<div className="ml-auto sm:hidden">
+						<button
+							onClick={openDropdownMenu}
+							className="btn bg-inherit border-none hover:bg-gray-700"
+						>
+							{getImage("Hamburger-Icon", 25, 25, "")}
+						</button>
+					</div>
 				</div>
-				<div className="ml-auto hidden sm:flex items-center">
-					<div className="mr-4">Log in</div>
-					<button className="btn bg-gray-600 text-white normal-case font-normal border-none hover:bg-gray-700">
-						Start free trial
-					</button>
+				<div className="mt-40">
+					<div className="sm:text-6xl text-4xl font-bold text-white">
+						A better way to
+					</div>
+					<div className="sm:text-6xl text-4xl font-bold text-indigo-400">
+						ship web apps
+					</div>
+					<div className="text-white w-96 mt-2">
+						Anim aute id magna aliqua ad ad non deserunt sunt. Qui
+						irure qui Lorem cupidatat commodo. Elit sunt amet fugiat
+						veniam occaecat fugiat.
+					</div>
+					<input
+						type="text"
+						placeholder="Enter your email"
+						className="mt-10 input input-bordered w-full max-w-xs"
+					/>
 				</div>
-				<div className="ml-auto sm:hidden">
-					<button
-						onClick={openDropdownMenu}
-						className="btn bg-inherit border-none hover:bg-gray-700"
-					>
-						{getImage("Hamburger-Icon", 25, 25, "")}
-					</button>
-				</div>
-			</div>
-			<div className="mt-40">
-				<div className="sm:text-6xl text-4xl font-bold text-white">
-					A better way to
-				</div>
-				<div className="sm:text-6xl text-4xl font-bold text-indigo-400">
-					ship web apps
-				</div>
-				<div className="text-white w-96 mt-2">
-					Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-					qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam
-					occaecat fugiat.
-				</div>
-				<input
-					type="text"
-					placeholder="Enter your email"
-					className="mt-10 input input-bordered w-full max-w-xs"
-				/>
 			</div>
 			{dropDownmenuOpen && (
 				<div className="dropdown-menu absolute top-0 left-0 w-full p-4">
